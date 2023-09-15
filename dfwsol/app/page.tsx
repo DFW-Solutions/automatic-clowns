521116c78cf6bdf90df46ce55c1c4b9b5746c1a9
Jonathan Self <jon.self@live.com>
2023-09-15 01:03:05 -0500

    set width of body

--- dfwsol/app/page.tsx
+++ dfwsol/app/page.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
-    <main className='w-4/5 mx-auto'>
+    <main className='w-2/5 mx-auto'>
       <h1 className={`${inter.className} text-4xl`}>Welcome to DFW Solutions</h1>
       <p className="text-2xl mt-4 pb-6">Content coming soon!</p>
 
